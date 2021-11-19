5e4be693d74e13a4386b4c0ec0bebd89ff919ee3
Joseph Brentnall <18679097@students.lincoln.ac.uk>
2021-11-19 09:57:53 +0000

    data.js now contains all points for each difficulty

--- assets/data/data.js
+++ assets/data/data.js
@@ -1,4 +1,18 @@
-export const hangmanWords = {
+export const hangman = {
+    points: {
+        0: {
+            v: 10,
+            c: 15
+        },
+        1: {
+            v: 20,
+            c: 30
+        },
+        2: {
+            v: 40,
+            c: 60 
+        }
+    },
     words: {
         easy: [
             'food',
